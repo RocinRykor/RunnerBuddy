30ccfca4e4e7c7d4583da7ee7f7f040a862e28c6
RocinRykor <briggs.steven369@gmail.com>
2021-07-16 18:58:03 -0700

    Pushing v0.1.1 Release

--- src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
+++ src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
@@ -2,5 +2,6 @@ package studio.rrprojects.runnerbuddy.constants;
 
 public class ProgramInfoConstants {
     public static final String PROGRAM_NAME = "RunnerBuddy ";
-    public static final String CURRENT_VERSION = "v 0.1.0";
+    public static final String CURRENT_VERSION = "v 0.1.1";
+    public static final String DATE_OF_LAST_CHANGE = "07/16/2021";
 }
