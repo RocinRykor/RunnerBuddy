afc16d18242fb82b5612d050b2fcfef8914a4709
RocinRykor <briggs.steven369@gmail.com>
2021-07-21 16:11:01 -0700

    Update to version 0.2.0
    -> Change the priority system

--- src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
+++ src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
@@ -2,6 +2,6 @@ package studio.rrprojects.runnerbuddy.constants;
 
 public class ProgramInfoConstants {
     public static final String PROGRAM_NAME = "RunnerBuddy ";
-    public static final String CURRENT_VERSION = "v 0.1.1";
-    public static final String DATE_OF_LAST_CHANGE = "07/16/2021";
+    public static final String CURRENT_VERSION = "v 0.2.0";
+    public static final String DATE_OF_LAST_CHANGE = "07/21/2021";
 }
