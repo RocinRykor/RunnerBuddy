2f7caa42f14a8f56015a3440d05e2957a304ad11
RocinRykor <briggs.steven369@gmail.com>
2021-07-24 18:34:40 -0700

    Updated the neme of the Priority A Adept to Magician Adept (from Adept, Way of the Magician) because the old name was far too fucking long

--- src/main/java/studio/rrprojects/runnerbuddy/constants/MagicConstants.java
+++ src/main/java/studio/rrprojects/runnerbuddy/constants/MagicConstants.java
@@ -2,8 +2,8 @@ package studio.rrprojects.runnerbuddy.constants;
 
 public class MagicConstants {
     public static final String FULL_MAGICIAN = "Full Magician";
-    public static final String ADEPT_WAY_OF_MAGIC = "Way of The Magician, Adept";
+    public static final String MAGICIAN_ADEPT = "Magician Adept";
     public static final String ADEPT = "Adept";
-    public static final String ASPECTED_MAGICIAN = "Elf";
+    public static final String ASPECTED_MAGICIAN = "Aspected Magician";
 
 }
