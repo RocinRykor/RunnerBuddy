dcaa50f50c4fe987b135a9aed8bf88d90f826be3
RocinRykor <briggs.steven369@gmail.com>
2021-07-30 18:20:35 -0700

    Added a dependency for a new placeholder Look And Feel.

--- src/main/java/studio/rrprojects/runnerbuddy/main/Main.java
+++ src/main/java/studio/rrprojects/runnerbuddy/main/Main.java
@@ -1,13 +1,25 @@
 package studio.rrprojects.runnerbuddy.main;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import studio.rrprojects.runnerbuddy.containers.character.CharacterContainer;
 import studio.rrprojects.runnerbuddy.gui.LaunchWindow;
 import studio.rrprojects.runnerbuddy.gui.popups.NewCharacterPriorityPopup;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        LaunchWindow window = new LaunchWindow();
 
+        UIManager.LookAndFeelInfo[] listLAF = UIManager.getInstalledLookAndFeels();
+
+
+        try {
+            UIManager.setLookAndFeel( new FlatDarculaLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+
+        new LaunchWindow();
 
         //Create a new CharacterContainer for use going forward.
         //CharacterContainer characterContainer = new CharacterContainer();
