d1978acc8d8456491e9d8e5d408e8896fbb12153
RocinRykor <briggs.steven369@gmail.com>
2021-08-22 15:30:04 -0700

    format change

--- src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
+++ src/main/java/studio/rrprojects/runnerbuddy/constants/ProgramInfoConstants.java
@@ -2,6 +2,6 @@ package studio.rrprojects.runnerbuddy.constants;
 
 public class ProgramInfoConstants {
     public static final String PROGRAM_NAME = "RunnerBuddy";
-    public static final String CURRENT_VERSION = " v 0.3";
+    public static final String CURRENT_VERSION = " v0.3";
     public static final String DATE_OF_LAST_CHANGE = "08/22/2021";
 }
