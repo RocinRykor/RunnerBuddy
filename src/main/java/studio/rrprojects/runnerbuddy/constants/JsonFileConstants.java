77aeef0b4b2beb668c2716ea6106514560bc529d
RocinRykor <briggs.steven369@gmail.com>
2021-10-15 10:57:18 -0700

    Why does this shit not wanna work on Windows!?

--- src/main/java/studio/rrprojects/runnerbuddy/constants/JsonFileConstants.java
+++ src/main/java/studio/rrprojects/runnerbuddy/constants/JsonFileConstants.java
@@ -1,5 +1,7 @@
 package studio.rrprojects.runnerbuddy.constants;
 
+import java.io.File;
+
 public class JsonFileConstants {
     //Character
     public static final String BLANK_CHARACTER = "blank_character_template.json";
@@ -15,12 +17,12 @@ public class JsonFileConstants {
 
     //Gear
     //Weapons
-    public static final String GEAR_PERSONAL = "Weapons/SR3E_personal_weapons.json";
-    public static final String GEAR_FIREARMS = "Weapons/SR3E_firearms.json";
-    public static final String GEAR_HEAVY_WEAPONS = "Weapons/SR3E_heavy_weapons.json";
-    public static final String GEAR_GRENADES = "Weapons/SR3E_grenades.json";
-    public static final String GEAR_IMPACT = "Weapons/SR3E_impact_projectile_weapons.json";
-    public static final String GEAR_ROCKETS = "Weapons/SR3E_rockets_and_missiles.json";
+    public static final String GEAR_PERSONAL = "Weapons" + File.separator + "SR3E_personal_weapons.json";
+    public static final String GEAR_FIREARMS = "Weapons" + File.separator + "SR3E_firearms.json";
+    public static final String GEAR_HEAVY_WEAPONS = "Weapons" + File.separator + "SR3E_heavy_weapons.json";
+    public static final String GEAR_GRENADES = "Weapons" + File.separator + "SR3E_grenades.json";
+    public static final String GEAR_IMPACT = "Weapons" + File.separator + "SR3E_impact_projectile_weapons.json";
+    public static final String GEAR_ROCKETS = "Weapons" + File.separator + "SR3E_rockets_and_missiles.json";
 
     //Armor
     public static final String GEAR_ARMOR = "SR3E_clothing_and_armor.json";
